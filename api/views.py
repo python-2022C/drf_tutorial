fe542c2e29f0a9963ddfa13b8b70695db48c9a50
ZARIFMAC <uzbzarif@gmail.com>
2022-11-15 18:24:52 +0500

    fix

--- api/views.py
+++ api/views.py
@@ -1,13 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import JsonResponse
 
 # Create your views here.
 @api_view(['GET'])
 def index(request):
     data = request.data
-    print(type(data))
-    print(data)
+
     return Response({'message': 'Hello, World!'})
 
 
