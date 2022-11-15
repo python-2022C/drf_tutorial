2339f3e201824b32635c98822c1064dc8d8d85e2
ZARIFMAC <uzbzarif@gmail.com>
2022-11-15 18:17:50 +0500

    get data

--- api/views.py
+++ api/views.py
@@ -1,6 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 # Create your views here.
 @api_view(['GET'])
 def index(request):
+    data = request.data
+    print(type(data))
+    print(data)
     return Response({'message': 'Hello, World!'})
+
+
+
