550ee29e88ac7300db2b4ec677bc78fc10e83de7
ZARIFMAC <uzbzarif@gmail.com>
2022-11-15 20:59:24 +0500

    app install

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import index
+from api.views import index,addStudent
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/',index),
+    path('addStudent/',addStudent)
 
 ]
