a9c14568953ae830905d702b33008590147ccd74
ZARIFMAC <uzbzarif@gmail.com>
2022-11-15 18:20:08 +0500

    home url

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import index
+from api.views import index,home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/',index),
+    path('',home),
 ]
