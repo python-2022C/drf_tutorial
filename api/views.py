53c6ee5de3e13ffc138bb0d44421192ef22203e6
ZARIFMAC <uzbzarif@gmail.com>
2022-11-15 18:30:03 +0500

    query string

--- api/views.py
+++ api/views.py
@@ -1,12 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+# Import django rest framework request
+from rest_framework.request import Request
 
 # Create your views here.
 @api_view(['GET'])
-def index(request):
-    data = request.data
+def index(request: Request):
+    data = request.query_params
+    print(data)
 
-    return Response({'message': 'Hello, World!'})
+
+    return Response({'result': data})
 
 
 
