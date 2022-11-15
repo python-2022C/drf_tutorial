9577f231583e27c3f853f54f00478f464e2f2c06
ZARIFMAC <uzbzarif@gmail.com>
2022-11-15 19:27:09 +0500

    add studetn

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=50)
-    score = models.IntegerField()
-    city = models.CharField(max_length=50)
+    score = models.IntegerField(default=0)
+    city = models.CharField(max_length=50, default='Jizzax',blank=True)
 
     def __str__(self):
         return self.name
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Import django rest framework request
 from rest_framework.request import Request
-
+from .models import Student
 # Create your views here.
 @api_view(['GET'])
 def index(request: Request):
@@ -12,6 +12,19 @@ def index(request: Request):
 
     return Response({'result': data})
 
+
+@api_view(['POST'])
+def addStudent(request: Request):
+    data = request.data
+    name = data.get('name','Ali')
+    score = data.get('score',0)
+    city = data.get('city','Jizzax')
+
+    student = Student(name=name,score=score,city=city)
+    student.save()   
+
+    
+    return Response({'result': data})
 
 
 
