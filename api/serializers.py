2d0f270e1268e6975474ca744de8723f96f130aa
ZARIFMAC <uzbzarif@gmail.com>
2022-11-17 17:52:54 +0500

    update

--- api/serializers.py
+++ api/serializers.py
@@ -11,7 +11,13 @@ class StudentSerializer(serializers.Serializer):
     city = serializers.CharField(max_length=50)
 
     def create(self, validated_data):
-        return Student.objects.create(**validated_data)          
-        )
+        return Student.objects.create(**validated_data)  #         
+        
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.score = validated_data.get('score', instance.score)
+        instance.city = validated_data.get('city', instance.city)
+        instance.save()
+        return instance
 
     
