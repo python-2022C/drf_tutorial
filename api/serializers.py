7ca0b6ffd425f56d37e345006554932f342398bf
ZARIFMAC <uzbzarif@gmail.com>
2022-11-17 17:49:32 +0500

    create

--- api/serializers.py
+++ api/serializers.py
@@ -3,14 +3,18 @@ from .models import Student
 from rest_framework import serializers
 
 class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=5,
+    name = serializers.CharField(max_length=50,
     required=True,
-    error_messages={
-        'required': 'Majburiy',
-        'max_length': 'Max 5 ta belgi dan olishi mumkin'
-    }
+   
     )
     score = serializers.IntegerField()
     city = serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        return Student.objects.create(
+            name=validated_data['name'],
+            score=validated_data['score'],
+            city=validated_data['city']            
+        )
 
     
